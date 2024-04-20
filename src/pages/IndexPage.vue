caaa362561c2cca02116d30422717003134ea8b1
Marcelo Sampaio <marcelosampaio@msn.com>
2024-04-20 11:01:23 -0300

    inicial

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page >
+    <div class="row flex-center">
+      <h1>PIO</h1>
+    </div>
+    <div class="row flex-center">
+      <h3>Plataforma de Integração Operacional</h3>
+    </div>
+
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
