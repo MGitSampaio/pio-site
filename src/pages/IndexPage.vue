a1bf3090a33d6f16ce39c4b079f2e120324856ac
MGitSampaio <marcelosampaio@msn.com>
2024-04-22 13:10:17 -0300

    build

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,22 @@
 <template>
-  <q-page >
-    <div class="row flex-center">
-      <h1>PIO</h1>
+  <q-page class="flex flex-center">
+    <div class="row flex flex-center">
+      <div class="flex flex-center">
+        <img
+          src="..\..\public\icons\favicon-512x512.png"
+          style="height: 300px; max-width: 300px"
+        />
+      </div>
+      <div>
+        <div class="flex flex-center"><h1>PIO</h1></div>
+        <div class="flex flex-center"><h3>Plataforma de Integração Operacional</h3></div>
+      </div>
     </div>
-    <div class="row flex-center">
-      <h3>Plataforma de Integração Operacional</h3>
-    </div>
-
   </q-page>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
